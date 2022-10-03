635cdcaffe35de13d9e1d0d8d38a592754514dc2
sma96 <masagu96@naver.com>
2022-10-03 16:52:00 +0900

    remove comments

--- PPUMPPAI/PPUMPPAI/Map/MapViewController.swift
+++ PPUMPPAI/PPUMPPAI/Map/MapViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class MapViewController: UIViewController {
     
     let label = UILabel()
-    let v = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,28 +20,19 @@ class MapViewController: UIViewController {
     
     func setup() {
         view.backgroundColor = .white
-        v.backgroundColor = .red
         
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Map"
         label.font = UIFont.preferredFont(forTextStyle: .title1)
         label.textAlignment = .center
-        
-        v.translatesAutoresizingMaskIntoConstraints = false
     }
     
     func layout() {
         view.addSubview(label)
-        view.addSubview(v)
         
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 10),
-
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            v.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            v.widthAnchor.constraint(equalToConstant: 100),
-            v.heightAnchor.constraint(equalToConstant: 100)
-//            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
 }
--- PPUMPPAI/PPUMPPAI/Utils/UIViewController+Utils.swift
+++ PPUMPPAI/PPUMPPAI/Utils/UIViewController+Utils.swift
@@ -9,31 +9,6 @@ import Foundation
 import UIKit
 
 extension UIViewController {
-    func setStatusBar() {
-//        let navBarAppearance = UINavigationBarAppearance()
-//        navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
-//        navBarAppearance.backgroundColor = appColor
-//        UINavigationBar.appearance().standardAppearance = navBarAppearance
-//        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
-        guard let statusSize = (UIApplication.shared.delegate as! AppDelegate).window?.windowScene?.statusBarManager?.statusBarFrame else {
-            return
-        }
-        
-        let style: UIBlurEffect.Style
-        let statusBar = UIView(frame: statusSize)
-        statusBar.backgroundColor = .clear
-        let blurEffect = UIBlurEffect(style: .dark)
-        let blurView = UIVisualEffectView(effect: blurEffect)
-        blurView.translatesAutoresizingMaskIntoConstraints = false
-        statusBar.insertSubview(blurView, at: 0)
-        blurView.heightAnchor.constraint(equalTo: statusBar.heightAnchor).isActive = true
-        blurView.widthAnchor.constraint(equalTo: statusBar.widthAnchor).isActive = true
-        
-//        statusBar.backgroundColor = appColor
-        
-        view.addSubview(statusBar)
-    }
-    
     func setTabBarImage(imageName: String, title: String) {
         let configuration = UIImage.SymbolConfiguration(scale: .large)
         let image = UIImage(systemName: imageName, withConfiguration: configuration)
