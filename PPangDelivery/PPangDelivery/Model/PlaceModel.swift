dd736d2e71c3b84c4c8fc02719767119377ff960
JJunghyunY <50796114+JJunghyunY@users.noreply.github.com>
2022-10-30 14:25:56 +0900

    :art: fix: split extensions to UIViewController & add assets and PlacesModel

--- PPangDelivery/PPangDelivery/Model/PlaceModel.swift
+++ PPangDelivery/PPangDelivery/Model/PlaceModel.swift
@@ -5,6 +5,16 @@
 //  Created by Chan on 2022/10/21.
 //
 
+import Foundation
+import FirebaseFirestore
+
 struct PlaceModel {
-	
+    var category : String
+    var createdTime : Timestamp
+    var dueTime : Timestamp
+    var id : Int
+    var location : GeoPoint
+    var numberOfMember : Int
+    var show : Bool
+    var userInputData : String
 }
